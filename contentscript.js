9ed86c9094107ccd4a4cc3610da5b640257909cd
Joseph Clark <joseph@thellama.us>
2017-01-31 14:08:01 -0500

    Code cleanup, updating styles + manifest

--- contentscript.js
+++ contentscript.js
@@ -2,16 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-function showLinkDestination(elem) {
-  //Built dom element w/ style
+function buildLinkDisplayContainer() {
   var linkDisplayContainer = document.createElement('div');
   linkDisplayContainer.setAttribute('id', 'link-display-div');
   linkDisplayContainer.setAttribute('class', 'link-display');
 
-  var linkHrefText = document.createTextNode(elem.attr('href'));
+  return linkDisplayContainer;
+}
+
+function showLinkDestination(elem) {
+  var linkHrefText = document.createTextNode(elem.attr('href')),
+      linkDisplayContainer = buildLinkDisplayContainer();
 
   linkDisplayContainer.appendChild(linkHrefText);
-
   document.body.appendChild(linkDisplayContainer);
 }
 
@@ -19,21 +22,23 @@ function hideLinkDestination(elem) {
   $('#link-display-div').remove();
 }
 
-$("a").hover(
-  function(e) {
-    console.log("mouse in");
-    showLinkDestination($(this));
-  },
-  function(e) {
-    console.log("mouse OUT");
-    hideLinkDestination($(this));
-  }
-);
+function initListeners() {
+  $('a').hover(
+    function(e) {
+      showLinkDestination($(this));
+    },
+    function(e) {
+      hideLinkDestination($(this));
+    }
+  );
 
-$(document).on('mousemove', function(e){
+  $(document).on('mousemove', function(e){
     $('#link-display-div').css({
-       position: 'absolute',
-       left:  e.pageX + 15,
-       top:   e.pageY + 15
-    });
-});
+        position: 'absolute',
+        left:  e.pageX + 15,
+        top:   e.pageY + 15
+      });
+  });
+}
+
+initListeners();
